84e0ace7f8400dfcfaca2943f89290dea288f9f5
Nastya <anastasia.kolobihina@yandex.ru>
2025-07-22 22:40:10 +0300

    Реазизовать ФЗ к 6 спринту

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,26 +2,91 @@ package manager;
 
 import model.Task;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final List<Task> historyStorage = new ArrayList<>();
+
+    private static class Node {
+        Task task;
+        Node prev;
+        Node next;
+
+        Node(Task task, Node prev, Node next) {
+            this.task = task;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private final Map<Integer, Node> historyMap = new HashMap<>();
+    private Node head;
+    private Node tail;
+
+
 
     @Override
     public void addTask(Task task) {
         if (Objects.isNull(task)) {
             return;
         }
+        remove(task.getId());
+        linkLast(task);
         historyStorage.add(new Task(task.getId(), task.getName(), task.getDescription(), task.getStatus()));
+
         if (historyStorage.size() > 10) {
-            historyStorage.remove(0);
+            historyStorage.removeFirst();
+        }
+
+    }
+
+    @Override
+    public void remove(int id) {
+        Node node = historyMap.get(id);
+        if (node != null) {
+            removeNode(node);
+            historyMap.remove(id);
+        }
+    }
+
+    private void linkLast(Task task) {
+        final Node newNode = new Node(task, tail, null);
+        if (tail == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        historyMap.put(task.getId(), newNode);
+    }
+
+    private void removeNode(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(historyStorage);
+        List<Task> history = new ArrayList<>();
+        Node current = head;
+        while (current != null) {
+            history.add(current.task);
+            current = current.next;
+        }
+        return history;
     }
 }
