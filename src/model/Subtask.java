47268ee6f9821169722bfecfd39bce759710aef7
Nastya <anastasia.kolobihina@yandex.ru>
2025-08-02 15:18:45 +0300

    Выполнено и поправлено ФЗ 7 спринта

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -8,6 +8,7 @@ public class Subtask extends Task {
         super(name, description, status);
         this.epicId = epicId;
     }
+
     public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
         super(id, name, description, status);
         this.epicId = epicId;
